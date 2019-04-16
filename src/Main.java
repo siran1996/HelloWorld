12341a1be20a9461272c2d5fb0cdcb38f7520946
siran1996 <527916897@qq.com>
2019-04-16 23:23:03 +0800

    test

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,63 @@
-import java.util.Random;
+import java.util.*;
 import java.lang.Math;
 
 public class Main {
 
-    public static void randomTest()
+    private static void randomTest()
     {
         Random r1=new Random(1);
         System.out.println(r1.nextInt(10));
 
     }
 
-    public static void mathTest()
+    private static void mathTest()
     {
         int a=-1;
         int b=Math.abs(a);
         System.out.println(b);
     }
 
-    public static void stringTest()
+    private static void stringTest()
     {
-        String s1=new String("123456");
+        String s1= "123456";
         char a=s1.charAt(4);
         System.out.println(a);
+    }
+
+    private static void listTest()
+    {
+        List<String> l= new ArrayList<>();
+        l.add("abc");
+        l.add("123");
+//        Iterable i= (Iterable) l.iterator();
+        for (String s:l
+             ) {
+            System.out.println(s);
+        }
+    }
+
+    private static void setTest()
+    {
+        Set<String> s=new HashSet<>();
+        s.add("abc");
+        s.add("123");
+        s.add("123");
+        for (String i:s
+             ) {
+            System.out.println(i);
+        }
+    }
+
+    private static void mapTest()
+    {
+        Map<Integer,String> m=new HashMap<>();
+        m.put(0,"abc");
+        m.put(1,"123");
+        for (Map.Entry entry:m.entrySet()
+             ) {
+            System.out.println("key="+entry.getKey()+" value="+entry.getValue());
+
+        }
     }
 
     public static void main(String[] args) {
@@ -29,5 +65,8 @@ public class Main {
         randomTest();
         mathTest();
         stringTest();
+        listTest();
+        setTest();
+        mapTest();
     }
 }
